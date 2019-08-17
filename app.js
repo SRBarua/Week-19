5ccc8ff99128517438ba69f82f97f98d115d1cbf
SRBarua <baruasudipta@gmail.com>
2019-08-17 02:15:28 -0600

    week 19

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        tasks: [
+            'breakfast',
+            'drop kids',
+            'attend class',
+            'lunch',
+            'do grocery',
+            'pickup kids',
+            'dinner',
+            'Sleep'
+        ]
+    },
+
+})
